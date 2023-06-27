eef3fd112b6d5b86ddf4c0bd2d001672c32d49b1
virokkkkkkk <virokkkkkkk07@icloud.com>
2023-06-27 14:03:36 +0300

    block1 [с богом 2]

--- accountlist.h
+++ accountlist.h
@@ -9,18 +9,18 @@ struct AccountItem {
     QString password;
 };
 
-class AccountList
+class AccountList //для хранения массива учетных записей
 {
 public:
-    AccountList();
-    bool loadJSON(QString filename);
+    AccountList(); //конструктор
+    bool loadJSON(QString filename); //для чтения из файла
 
-    void appendItem(AccountItem item);
-    AccountItem getItem(int index);
-    int size();
+    void appendItem(AccountItem item); //добавление элемента
+    AccountItem getItem(int index); //метод для получения элемента
+    int size(); //возвращает количество эл-в вектора
 
 private:
-    QVector<AccountItem> mItems;
+    QVector<AccountItem> mItems; //вектор из объектов <AccountItem>
 };
 
 #endif // ACCOUNTLIST_H
